1ab17f53ae50a5dcabd786d62db8caaa303f0d48
unknown <tejaswinirao1605@gmail.com>
2026-04-23 14:59:00 -0400

    added add task functionality

---
+++ todo/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Task
+
+# Create your views here.
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
+
+
+    # return HttpResponse('the form is submited')
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,8 +1,15 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from todo.models import Task
 
 
 
 def home(request):
-    return render(request,'home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context = {
+        'tasks': tasks,
+    }
+
+    # print(task)
+    return render(request,'home.html',context)
     # return HttpResponse('<h1>Homepage<h1/>')
