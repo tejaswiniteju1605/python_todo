39a3201404622bf3227fa1a868f1fa8f45fe0368
unknown <tejaswinirao1605@gmail.com>
2026-04-23 17:01:13 -0400

    todo app with curd operations

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+   # add task
     path('addTask/',views.addTask,name='addTask'),
+    # mark as done
+    path('mark_as_done /<int:pk>/',views.mark_as_done,name="mark_as_done"),
+    #mark as undone 
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone,name="mark_as_undone"),
+
+    #edit  feature
+    path('edit_task/<int:pk>',views.edit_task,name="edit_task"),
+
+     #delete  task
+    path('delete_task/<int:pk>',views.delete_task,name="delete_task")
+
+
+
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -6,8 +6,12 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+    # print(completed_tasks)
     context = {
         'tasks': tasks,
+        'completed_tasks':completed_tasks,
     }
 
     # print(task)
